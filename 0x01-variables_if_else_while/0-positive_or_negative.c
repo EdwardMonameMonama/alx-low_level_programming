adff6d099e90f4b168a0fa80445b91d9841594ff
EdwardMonameMonama <monamaedward@yahoo.com>
2023-12-18 11:51:20 -0800

    Confessions Part 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,14 +10,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if( n > 0 ) {
-		printf("n is positive\n");
+
+	if(n>0) 
+	{
+		printf("%d is positive\n", n);
 	}
-	else if ( n == 0 ) {
-		printf("n is zero\n");
+	else if(n==0)
+       	{
+		printf("%d is zero\n", n);
 	}
-	else {
-		printf("n is negative\n");
+	else
+       	{
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
