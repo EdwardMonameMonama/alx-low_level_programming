fadc1e5053e461f108c7c536eee293decf3eca52
EdwardMonameMonama <monamaedward@yahoo.com>
2023-12-26 14:09:18 -0800

    imithandazo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char e;
+	for (e = 'a'; e <= 'z'; e++)
+		putchar(e);
+	putchar('\n');
+	return (0);
+}
