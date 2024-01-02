1991b5305e5ebbac6abec0e999cd8351a63f172b
EdwardMonameMonama <monamaedward@yahoo.com>
2024-01-02 08:37:36 -0800

    Stephen Sanchez - Until i found her

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -  a function that reverses a string
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int len = 0, index = 0;
+	char tmp;
+
+	while (s[index++])
+		len++;
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index -1] = tmp;
+	}
+}
